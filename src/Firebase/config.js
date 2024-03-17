7a62b1748406416eab21a7fe2b8695c3aad5778a
sema-demir <semademir0505@gmail.com>
2024-03-17 13:34:12 +0300

    chat page özellikleri

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "Firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +24,5 @@ export const auth = getAuth(app);
 
 // google saglayıcsının kurulumu
 export const provider = new GoogleAuthProvider();
+// veritabanın referansını alma
+export const db = getFirestore(app);
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,13 +1,71 @@
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+} from "firebase/firestore";
+import { auth, db } from "../Firebase/config";
+import { useEffect, useState } from "react";
+
 const ChatPage = ({ room, setRoom }) => {
+  const [messages, setMessages] = useState([]);
+
+  // mesaj gönderme fonksiyonu
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    // kolleksiyonun referansını alma
+    const messagesCol = collection(db, "messages");
+
+    // kolleksiyona yeni döküman ekle
+    await addDoc(messagesCol, {
+      text: e.target[0].value,
+      room,
+      author: {
+        id: auth.currentUser.uid,
+        name: auth.currentUser.displayName,
+        photo: auth.currentUser.photoURL,
+      },
+      createdAt: serverTimestamp(),
+    });
+    // formu sıfırla
+    e.target.reset();
+  };
+
+  // mevcut odada gönderilen mesajları anlık olarak alır
+  useEffect(() => {
+    // kolleksiyonun referansını al
+    const messagesCol = collection(db, "messages");
+
+    // anlık olarak bir kolleksiyondaki değişimleri izler
+    // kolleksiyon her değiştiğinde verdiğimiz fonkisyon ile kolleksiyondaki bütün dökümanlara erişiriz yani yeni mesajlara erişiriz
+
+    onSnapshot(messagesCol, (snapshot) => {
+      // verilerin geçici olarak tutulacağı boş dizi oluştur
+      const tempMsg = [];
+
+      // dökümanlar dönüp verilerine erişdik
+      snapshot.docs.forEach((doc) => {
+        tempMsg.push(doc.data());
+      });
+      // mesajları state e aktar
+      setMessages(tempMsg);
+    });
+  }, []);
+
   return (
     <div className="chat-page">
       <header>
-        <p></p>
-        <p></p>
-        <button></button>
+        <p>{auth.currentUser?.displayName}</p>
+        <p>{room}</p>
+        <button onClick={() => setRoom(null)}>Farklı Oda</button>
       </header>
-      <main></main>
-      <form></form>
+      <main>
+        <p>mesaj</p>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input type="text" required placeholder="mesajınızı yazınız" />
+        <button>Gönder</button>
+      </form>
     </div>
   );
 };
